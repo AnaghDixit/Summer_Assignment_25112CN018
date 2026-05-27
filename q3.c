116bc333228256e097a8e071a2a97166a35feef5
AnaghDixit <thecoderanagh@gmail.com>
2026-05-28 01:35:03 +0530

    commiting third question

--- q3.c
+++ q3.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-int main ()
+
+int main(void)
 {
- int n,i,fact=1;   
-    
- printf("Enter the number:");
- scanf("%d",&n);
- 
- for(i=1;i<=n;i++)
- {
-  fact = fact*i;   
- }
-  printf("Factorial is %d",fact); 
-  
-  return 0;  
-}   
+    int n, i, fact = 1;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("Factorial is %d", fact);
+
+    return 0;
+}
+   
